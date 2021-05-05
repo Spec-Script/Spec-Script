9bf7949a1cab3880bd740eb9350a43546b4cf1fb
Glaukio <70211498+glaukiol1@users.noreply.github.com>
2021-05-05 13:43:45 +0000

    Update complier.js

--- complier.js
+++ complier.js
@@ -4,4 +4,4 @@ const Compiler=require('./complier/main')
 const fileToCompile=process.argv[process.argv.length-1].substring(2,process.argv[process.argv.length-1].length)
 const to = process.argv[process.argv.length-2].substring(2,process.argv[process.argv.length-2].length)
 
-console.log(Compiler.Complie(fileToCompile,to, 'browser'))
+console.log(Compiler.Complie(fileToCompile,to, 'browser')) // change 'browser' to 'node' if you want the script to compile in a nodejs env
